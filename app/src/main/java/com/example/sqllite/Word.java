7150dc5f1bbe598843b23b2a8070ac12e8df54e5
Valerii <teslav77@gmail.com>
2023-03-19 00:26:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sqllite/Word.java
@@ -0,0 +1,33 @@
+package com.example.sqllite;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")// на основе этого класса создается таблица
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+
+    public void setWord(@NonNull String mWord) {
+        this.mWord = mWord;
+    }
+}
